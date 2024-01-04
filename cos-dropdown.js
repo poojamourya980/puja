0719848a89a78861248422d5fe38bdf2c8df62a5
poojamourya980 <155438243+poojamourya980@users.noreply.github.com>
2024-01-04 15:23:42 +0530

    initialization

---
+++ cos-dropdown.js
@@ -0,0 +1,221 @@
+const handleDropdownClicked = (event) => {
+    event.stopPropagation();
+    const dropdown = document.getElementById("dropdown");
+    toggleDropdown(!dropdown.classList.contains("open"));
+  };
+  
+  const toggleDropdown = (shouldOpen) => {
+    const dropdown = document.getElementById("dropdown");
+    const icon = document.getElementById("icon");
+  
+    if (shouldOpen) {
+      dropdown.classList.add("open");
+    } else {
+      dropdown.classList.remove("open");
+    }
+  
+  
+  };
+  
+  document.body.addEventListener("click", () => toggleDropdown());
+  
+  
+  
+  const handleDropdownClicked2 = (event) => {
+    event.stopPropagation();
+    const dropdown = document.getElementById("dropdown1");
+    toggleDropdown2(!dropdown.classList.contains("open"));
+  };
+  
+  const toggleDropdown2 = (shouldOpen) => {
+    const dropdown = document.getElementById("dropdown1");
+    const icon = document.getElementById("icon");
+  
+    if (shouldOpen) {
+      dropdown.classList.add("open");
+    } else {
+      dropdown.classList.remove("open");
+    }
+  
+  
+  };
+  
+  document.body.addEventListener("click", () => toggleDropdown2());
+  
+  
+  const handleDropdownClicked3 = (event) => {
+    event.stopPropagation();
+    const dropdown = document.getElementById("dropdown2");
+    toggleDropdown3(!dropdown.classList.contains("open"));
+  };
+  
+  const toggleDropdown3 = (shouldOpen) => {
+    const dropdown = document.getElementById("dropdown2");
+    const icon = document.getElementById("icon");
+  
+    if (shouldOpen) {
+      dropdown.classList.add("open");
+    } else {
+      dropdown.classList.remove("open");
+    }
+  
+  
+  };
+  
+  document.body.addEventListener("click", () => toggleDropdown3());
+  
+  
+  const handleDropdownClicked4 = (event) => {
+    event.stopPropagation();
+    const dropdown = document.getElementById("dropdown3");
+    toggleDropdown4(!dropdown.classList.contains("open"));
+  };
+  
+  const toggleDropdown4 = (shouldOpen) => {
+    const dropdown = document.getElementById("dropdown3");
+    const icon = document.getElementById("icon");
+  
+    if (shouldOpen) {
+      dropdown.classList.add("open");
+    } else {
+      dropdown.classList.remove("open");
+    }
+  
+  
+  };
+  
+  document.body.addEventListener("click", () => toggleDropdown4());
+  
+  const handleDropdownClicked5 = (event) => {
+    event.stopPropagation();
+    const dropdown = document.getElementById("dropdown4");
+    toggleDropdown5(!dropdown.classList.contains("open"));
+  };
+  
+  const toggleDropdown5 = (shouldOpen) => {
+    const dropdown = document.getElementById("dropdown4");
+    const icon = document.getElementById("icon");
+  
+    if (shouldOpen) {
+      dropdown.classList.add("open");
+    } else {
+      dropdown.classList.remove("open");
+    }
+  
+  
+  };
+  
+  document.body.addEventListener("click", () => toggleDropdown5());
+  
+
+  const handleDropdownClicked6 = (event) => {
+    event.stopPropagation();
+    const dropdown = document.getElementById("dropdown5");
+    toggleDropdown6(!dropdown.classList.contains("open"));
+  };
+  
+  const toggleDropdown6 = (shouldOpen) => {
+    const dropdown = document.getElementById("dropdown5");
+    const icon = document.getElementById("icon");
+  
+    if (shouldOpen) {
+      dropdown.classList.add("open");
+    } else {
+      dropdown.classList.remove("open");
+    }
+  
+  
+  };
+  
+  document.body.addEventListener("click", () => toggleDropdown6());
+  
+
+
+  const handleDropdownClicked7 = (event) => {
+    event.stopPropagation();
+    const dropdown = document.getElementById("dropdown6");
+    toggleDropdown7(!dropdown.classList.contains("open"));
+  };
+  
+  const toggleDropdown7 = (shouldOpen) => {
+    const dropdown = document.getElementById("dropdown6");
+    const icon = document.getElementById("icon");
+  
+    if (shouldOpen) {
+      dropdown.classList.add("open");
+    } else {
+      dropdown.classList.remove("open");
+    }
+  
+  
+  };
+  
+  document.body.addEventListener("click", () => toggleDropdown7());
+  
+
+  
+  const handleDropdownClicked8 = (event) => {
+    event.stopPropagation();
+    const dropdown = document.getElementById("dropdown7");
+    toggleDropdown8(!dropdown.classList.contains("open"));
+  };
+  
+  const toggleDropdown8 = (shouldOpen) => {
+    const dropdown = document.getElementById("dropdown7");
+    const icon = document.getElementById("icon");
+  
+    if (shouldOpen) {
+      dropdown.classList.add("open");
+    } else {
+      dropdown.classList.remove("open");
+    }
+  
+  
+  };
+  
+  document.body.addEventListener("click", () => toggleDropdown8());
+  
+
+  const handleDropdownClicked9 = (event) => {
+    event.stopPropagation();
+    const dropdown = document.getElementById("dropdown8");
+    toggleDropdown9(!dropdown.classList.contains("open"));
+  };
+  
+  const toggleDropdown9 = (shouldOpen) => {
+    const dropdown = document.getElementById("dropdown8");
+    const icon = document.getElementById("icon");
+  
+    if (shouldOpen) {
+      dropdown.classList.add("open");
+    } else {
+      dropdown.classList.remove("open");
+    }
+  
+  
+  };
+  
+  document.body.addEventListener("click", () => toggleDropdown9());
+
+
+
+  const handleDropdownClicked10 = (event) => {
+    event.stopPropagation();
+    const dropdown = document.getElementById("dropdown9");
+    toggleDropdown10(!dropdown.classList.contains("open"));
+  };
+  
+  const toggleDropdown10 = (shouldOpen) => {
+    const dropdown = document.getElementById("dropdown9");
+    const icon = document.getElementById("icon");
+  
+    if (shouldOpen) {
+      dropdown.classList.add("open");
+    } else {
+      dropdown.classList.remove("open");
+    }
+  
+  
+  };
+  
+  document.body.addEventListener("click", () => toggleDropdown10());
